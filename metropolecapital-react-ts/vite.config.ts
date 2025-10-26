815962fbe9cb0b0f78bfe559cd173ce6011cc577
intrafellow <145547700+intrafellow@users.noreply.github.com>
2025-10-26 18:57:14 +0300

    Fix 404 error on Vercel

--- metropolecapital-react-ts/vite.config.ts
+++ metropolecapital-react-ts/vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'node:url';
 const r = (p: string) => fileURLToPath(new URL(p, import.meta.url));
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
